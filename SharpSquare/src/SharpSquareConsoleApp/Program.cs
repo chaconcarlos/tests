a286d0208c18b5cb824720b4fd5ba2f8d25970ad
Carlos Chacon <chacon.carlosj@gmail.com>
2013-10-10 18:25:06 -0500

    * First commit of SharpSquare Test.

---
+++ SharpSquare/src/SharpSquareConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Program.cs" company="Smartmatic Corp.">
+//   Smartmatic Corp. All Rights Reserved.
+// </copyright>
+// <author>carlos.rivas</author>
+// <date>10/10/2013 4:31:17 PM</date>
+//
+// Confidential Information of Smartmatic Labs. Not for disclosure or distribution
+// prior written consent. This software contains code, techniques and know-how which
+// is confidential and proprietary to Smartmatic.
+//
+// Use of this software is subject to the terms of an end user license agreement.
+
+using System;
+using System.Collections.Generic;
+using Igloo.SharpSquare.Core;
+using Igloo.SharpSquare.Entities;
+
+namespace SharpSquareConsoleApp
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string      clientId     = "CLIENT_ID";
+            string      clientSecret = "CLIEND_SECRET";
+            //string      redirectUri  = "REDIRECT_URI";
+            SharpSquare sharpSquare  = new SharpSquare(clientId, clientSecret);
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            parameters.Add("ll", "8.98225, -79.50955");
+            parameters.Add("radius", "8.98225, -79.50955");
+
+            List<Venue> venues = sharpSquare.SearchVenues(parameters);
+
+            foreach (Venue venue in venues)
+            {
+                Console.Out.WriteLine(venue.id + " " + venue.name + " " + venue.mayor);
+            }
+        }
+    }
+}
---
+++ SharpSquare/src/SharpSquareConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="assemblyinfo.cs" company="Smartmatic Corp.">
+//   Smartmatic Corp. All Rights Reserved.
+// </copyright>
+// <author>carlos.rivas</author>
+// <date>10/10/2013 4:31:17 PM</date>
+//
+// Confidential Information of Smartmatic Labs. Not for disclosure or distribution
+// prior written consent. This software contains code, techniques and know-how which
+// is confidential and proprietary to Smartmatic.
+//
+// Use of this software is subject to the terms of an end user license agreement.
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpSquareConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Smartmatic Labs")]
+[assembly: AssemblyProduct("SharpSquareConsoleApp")]
+[assembly: AssemblyCopyright("Copyright © Smartmatic Labs 2013")]
+[assembly: ComVisible(false)]
+[assembly: Guid("25585b2c-a61c-4ef3-95d8-1be21c30715b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
